e3359266998b445fe04f532bf770a965e3f497b0
Ruslan Zakirov <ruz@bestpractical.com>
2009-10-16 04:27:43 +0400

    initial commit

---
+++ lib/RT/Extension/MoveRules.pm
@@ -0,0 +1,30 @@
+package RT::Extension::MoveRules;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+RT::Extension::MoveRules - control ticket movements between queues
+
+=head1 DESCRIPTION
+
+If you move tickets between queues a lot then probably you want
+to control when ticket moves where.
+
+=cut
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>Ruslan.Zakirov@gmail.comE<gt>
+
+=cut
+
+1;
