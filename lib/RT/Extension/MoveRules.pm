623ec0e5cc59a627b3d87633314cf928dbc6b36b
Ruslan Zakirov <ruz@bestpractical.com>
2009-11-07 12:57:33 +0700

    mass update

--- lib/RT/Extension/MoveRules.pm
+++ lib/RT/Extension/MoveRules.pm
@@ -1,8 +1,8 @@
-package RT::Extension::MoveRules;
-
-use 5.008;
+use 5.008003;
 use strict;
 use warnings;
+
+package RT::Extension::MoveRules;
 
 our $VERSION = '0.01';
 
@@ -13,9 +13,95 @@ RT::Extension::MoveRules - control ticket movements between queues
 =head1 DESCRIPTION
 
 If you move tickets between queues a lot then probably you want
-to control when ticket moves where.
+to control this process. This extension allows you to configure
+rules which are required to move a ticket from a queue to another
+queue, for example if custom field X is equal to Y then a ticket
+can be moved from queue A to B. As well you can define which
+fields should be set before move.
 
 =cut
+
+$RT::Config::META{'MoveRules'} = {
+    Type => 'ARRAY',
+};
+
+
+{ my $cache;
+sub Config {
+    my $self = shift;
+    return $cache if $cache;
+
+    $cache = {};
+    foreach my $rule ( RT->Config->Get('MoveRules') ) {
+        $cache->{ lc $rule->{'From'} }{ lc $rule->{'To'} }
+            = { %$rule };
+    }
+    return $cache;
+} }
+
+use RT::Condition::Complex;
+
+
+sub Check {
+    my $self = shift;
+    my %args = (
+        From => undef, To => undef,
+        Ticket => undef,
+        @_
+    );
+
+    my $config = $self->Config->{ lc $args->{'From'}->Name }
+        ->{ lc $args->{'To'}->Name };
+    unless ( $config ) {
+        return (0, $args->{'Ticket'}->loc('Ticket move to that queue is not allowed'));
+    }
+
+    if ( my $rule = $config->{'Rule'} ) {
+        my $cond = RT::Condition::Complex->new(
+            TicketObj => $args{'Ticket'},
+            TransactionObj => $args{'Transaction'}
+        );
+        my $res = $cond->Solve(
+            $rule, From => $args{'From'}, To => $args{'To'}
+        );
+        return (0, $args->{'Ticket'}->loc('Ticket with this properties can not be moved to that queue');
+    }
+    return 1;
+}
+
+use RT::Ticket;
+package RT::Ticket;
+
+{
+    my $old = \&SetQueue;
+    sub SetQueue {
+        my $self = shift;
+        return $old->( $self, @_ ) unless $self->Type eq 'ticket';
+
+        my $new_id = shift;
+
+        # we have to duplicate some code
+
+        my $new = RT::Queue->new( $self->CurrentUser );
+        $new->Load( $new_id );
+        unless ( $new->id ) {
+            return (0, $self->loc("That queue does not exist"));
+        }
+
+        my $old = $self->QueueObj;
+        if ( $old->id == $new->id ) {
+            return ( 0, $self->loc('That is the same value') );
+        }
+
+        my ($status, $msg) = RT::Extension::MoveRules->Check(
+            From => $old, To => $new,
+            Ticket => $self,
+        );
+        return ($status, $msg) unless $status;
+
+        return $old->( $self, $new_id, @_ );
+    }
+}
 
 =head1 LICENSE
 
